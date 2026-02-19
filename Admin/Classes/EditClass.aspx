cb155f58a0f6fb646ec146c015b7d5ce8996a6c5
Tariq Manzar <stariq1729@gmail.com>
2026-02-19 14:48:27 +0530

    added edit class

--- Admin/Classes/EditClass.aspx
+++ Admin/Classes/EditClass.aspx
@@ -2,4 +2,48 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div class="form-card shadow-sm">
+
+        <h4 class="form-title">Edit Class</h4>
+
+        <asp:HiddenField ID="hfClassId" runat="server" />
+
+        <div class="mb-3">
+            <label class="form-label">Select Board</label>
+            <asp:DropDownList ID="ddlBoard" runat="server"
+                CssClass="form-select">
+            </asp:DropDownList>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Class Name</label>
+            <asp:TextBox ID="txtClassName" runat="server"
+                CssClass="form-control">
+            </asp:TextBox>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Display Order</label>
+            <asp:TextBox ID="txtDisplayOrder" runat="server"
+                CssClass="form-control"
+                TextMode="Number">
+            </asp:TextBox>
+        </div>
+
+        <div class="form-check mb-3">
+            <asp:CheckBox ID="chkIsActive" runat="server"
+                CssClass="form-check-input" />
+            <label class="form-check-label">Is Active</label>
+        </div>
+
+        <asp:Button ID="btnUpdate" runat="server"
+            Text="Update Class"
+            CssClass="btn btn-primary"
+            OnClick="btnUpdate_Click" />
+
+        <asp:Label ID="lblMessage" runat="server"
+            CssClass="text-danger d-block mt-3">
+        </asp:Label>
+
+    </div>
 </asp:Content>
--- Admin/Classes/EditClass.aspx.cs
+++ Admin/Classes/EditClass.aspx.cs
@@ -1,17 +1,130 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace StudyIsleWeb.Admin.Classes
 {
     public partial class EditClass : System.Web.UI.Page
     {
+        private readonly string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadBoards();
 
+                if (Request.QueryString["id"] != null)
+                {
+                    int classId = Convert.ToInt32(Request.QueryString["id"]);
+                    LoadClass(classId);
+                }
+                else
+                {
+                    Response.Redirect("ManageClasses.aspx");
+                }
+            }
+        }
+
+        private void LoadBoards()
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "SELECT BoardId, BoardName FROM Boards WHERE IsActive = 1";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                ddlBoard.DataSource = dt;
+                ddlBoard.DataTextField = "BoardName";
+                ddlBoard.DataValueField = "BoardId";
+                ddlBoard.DataBind();
+            }
+        }
+
+        private void LoadClass(int classId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "SELECT * FROM Classes WHERE ClassId=@ClassId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@ClassId", classId);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    hfClassId.Value = reader["ClassId"].ToString();
+                    ddlBoard.SelectedValue = reader["BoardId"].ToString();
+                    txtClassName.Text = reader["ClassName"].ToString();
+                    txtDisplayOrder.Text = reader["DisplayOrder"].ToString();
+                    chkIsActive.Checked = Convert.ToBoolean(reader["IsActive"]);
+                }
+                else
+                {
+                    Response.Redirect("ManageClasses.aspx");
+                }
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int classId = Convert.ToInt32(hfClassId.Value);
+            int boardId = Convert.ToInt32(ddlBoard.SelectedValue);
+            string className = txtClassName.Text.Trim();
+            int displayOrder = Convert.ToInt32(txtDisplayOrder.Text);
+            bool isActive = chkIsActive.Checked;
+
+            if (!BoardSupportsClasses(boardId))
+            {
+                lblMessage.Text = "This board does not support class layer.";
+                return;
+            }
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = @"UPDATE Classes
+                                 SET BoardId=@BoardId,
+                                     ClassName=@ClassName,
+                                     DisplayOrder=@DisplayOrder,
+                                     IsActive=@IsActive
+                                 WHERE ClassId=@ClassId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@ClassId", classId);
+                cmd.Parameters.AddWithValue("@BoardId", boardId);
+                cmd.Parameters.AddWithValue("@ClassName", className);
+                cmd.Parameters.AddWithValue("@DisplayOrder", displayOrder);
+                cmd.Parameters.AddWithValue("@IsActive", isActive);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            Response.Redirect("ManageClasses.aspx");
+        }
+
+        private bool BoardSupportsClasses(int boardId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "SELECT HasClassLayer FROM Boards WHERE BoardId=@BoardId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@BoardId", boardId);
+
+                return Convert.ToBoolean(cmd.ExecuteScalar());
+            }
         }
     }
 }
