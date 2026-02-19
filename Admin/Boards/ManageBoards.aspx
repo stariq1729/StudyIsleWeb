c675244d5093c0c606c4ef9d978dc8b52bfe1170
Tariq Manzar <stariq1729@gmail.com>
2026-02-19 13:07:23 +0530

    added manage boards page in admin dashboard

--- Admin/Boards/ManageBoards.aspx
+++ Admin/Boards/ManageBoards.aspx
@@ -2,4 +2,37 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+      <div class="d-flex justify-content-between align-items-center mb-3">
+        <h4 class="page-title">Manage Boards</h4>
+        <a href="AddBoard.aspx" class="btn btn-primary btn-add">+ Add Board</a>
+    </div>
+
+    <asp:GridView ID="gvBoards" runat="server" 
+        AutoGenerateColumns="False" 
+        CssClass="table table-bordered"
+        OnRowCommand="gvBoards_RowCommand">
+
+        <Columns>
+
+            <asp:BoundField DataField="BoardId" HeaderText="ID" />
+
+            <asp:BoundField DataField="BoardName" HeaderText="Board Name" />
+
+            <asp:BoundField DataField="HasClassLayer" HeaderText="Has Class Layer" />
+
+            <asp:TemplateField HeaderText="Status">
+                <ItemTemplate>
+                    <asp:LinkButton ID="btnToggle" runat="server"
+                        CommandName="ToggleActive"
+                        CommandArgument='<%# Eval("BoardId") %>'
+                        CssClass='<%# Convert.ToBoolean(Eval("IsActive")) ? "badge badge-active text-white p-2" : "badge badge-inactive text-white p-2" %>'>
+                        <%# Convert.ToBoolean(Eval("IsActive")) ? "Active" : "Inactive" %>
+                    </asp:LinkButton>
+                </ItemTemplate>
+            </asp:TemplateField>
+
+        </Columns>
+
+    </asp:GridView>
 </asp:Content>
--- Admin/Boards/ManageBoards.aspx.cs
+++ Admin/Boards/ManageBoards.aspx.cs
@@ -1,17 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace StudyIsleWeb.Admin.Boards
 {
     public partial class ManageBoards : System.Web.UI.Page
     {
+        private readonly string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadBoards();
+            }
+        }
 
+        private void LoadBoards()
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "SELECT BoardId, BoardName, HasClassLayer, IsActive FROM Boards";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvBoards.DataSource = dt;
+                gvBoards.DataBind();
+            }
+        }
+
+        protected void gvBoards_RowCommand(object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "ToggleActive")
+            {
+                int boardId = Convert.ToInt32(e.CommandArgument);
+                ToggleBoardStatus(boardId);
+                LoadBoards();
+            }
+        }
+
+        private void ToggleBoardStatus(int boardId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "UPDATE Boards SET IsActive = ~IsActive WHERE BoardId=@BoardId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@BoardId", boardId);
+
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
