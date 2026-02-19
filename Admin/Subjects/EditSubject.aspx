fce4ddf7bdcfd1b35f68a5aa9243bad3a1a87041
Tariq Manzar <stariq1729@gmail.com>
2026-02-19 15:16:13 +0530

    added editsubject button and working for subjects

---
+++ Admin/Subjects/EditSubject.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="EditSubject.aspx.cs" Inherits="StudyIsleWeb.Admin.Subjects.EditSubject" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+      <div class="form-card shadow-sm">
+
+        <h4 class="form-title">Edit Subject</h4>
+
+        <asp:HiddenField ID="hfSubjectId" runat="server" />
+
+        <div class="mb-3">
+            <label class="form-label">Select Board</label>
+            <asp:DropDownList ID="ddlBoard" runat="server"
+                CssClass="form-select"
+                AutoPostBack="true"
+                OnSelectedIndexChanged="ddlBoard_SelectedIndexChanged">
+            </asp:DropDownList>
+        </div>
+
+        <div class="mb-3" id="classContainer" runat="server">
+            <label class="form-label">Select Class</label>
+            <asp:DropDownList ID="ddlClass" runat="server"
+                CssClass="form-select">
+            </asp:DropDownList>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Subject Name</label>
+            <asp:TextBox ID="txtSubjectName" runat="server"
+                CssClass="form-control">
+            </asp:TextBox>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Slug</label>
+            <asp:TextBox ID="txtSlug" runat="server"
+                CssClass="form-control">
+            </asp:TextBox>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Description</label>
+            <asp:TextBox ID="txtDescription" runat="server"
+                CssClass="form-control"
+                TextMode="MultiLine">
+            </asp:TextBox>
+        </div>
+
+        <div class="form-check mb-3">
+            <asp:CheckBox ID="chkIsActive" runat="server"
+                CssClass="form-check-input" />
+            <label class="form-check-label">Is Active</label>
+        </div>
+
+        <asp:Button ID="btnUpdate" runat="server"
+            Text="Update Subject"
+            CssClass="btn btn-primary"
+            OnClick="btnUpdate_Click" />
+
+        <asp:Label ID="lblMessage" runat="server"
+            CssClass="text-danger d-block mt-3">
+        </asp:Label>
+
+    </div>
+</asp:Content>
---
+++ Admin/Subjects/EditSubject.aspx.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StudyIsleWeb.Admin.Subjects
+{
+    public partial class EditSubject : System.Web.UI.Page
+    {
+        private readonly string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadBoards();
+
+                if (Request.QueryString["id"] != null)
+                {
+                    int subjectId = Convert.ToInt32(Request.QueryString["id"]);
+                    LoadSubject(subjectId);
+                }
+                else
+                {
+                    Response.Redirect("ManageSubjects.aspx");
+                }
+            }
+        }
+
+        private void LoadBoards()
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "SELECT BoardId, BoardName FROM Boards WHERE IsActive=1";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                ddlBoard.DataSource = dt;
+                ddlBoard.DataTextField = "BoardName";
+                ddlBoard.DataValueField = "BoardId";
+                ddlBoard.DataBind();
+            }
+        }
+
+        private void LoadSubject(int subjectId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "SELECT * FROM Subjects WHERE SubjectId=@SubjectId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@SubjectId", subjectId);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    hfSubjectId.Value = reader["SubjectId"].ToString();
+                    ddlBoard.SelectedValue = reader["BoardId"].ToString();
+                    txtSubjectName.Text = reader["SubjectName"].ToString();
+                    txtSlug.Text = reader["Slug"].ToString();
+                    txtDescription.Text = reader["Description"].ToString();
+                    chkIsActive.Checked = Convert.ToBoolean(reader["IsActive"]);
+                }
+            }
+
+            ddlBoard_SelectedIndexChanged(null, null);
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "SELECT ClassId FROM Subjects WHERE SubjectId=@SubjectId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@SubjectId", subjectId);
+
+                object classId = cmd.ExecuteScalar();
+
+                if (classId != DBNull.Value && classId != null)
+                {
+                    ddlClass.SelectedValue = classId.ToString();
+                }
+            }
+        }
+
+        protected void ddlBoard_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int boardId = Convert.ToInt32(ddlBoard.SelectedValue);
+
+            if (BoardSupportsClasses(boardId))
+            {
+                LoadClasses(boardId);
+                classContainer.Visible = true;
+            }
+            else
+            {
+                classContainer.Visible = false;
+            }
+        }
+
+        private void LoadClasses(int boardId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "SELECT ClassId, ClassName FROM Classes WHERE BoardId=@BoardId AND IsActive=1";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@BoardId", boardId);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                ddlClass.DataSource = dt;
+                ddlClass.DataTextField = "ClassName";
+                ddlClass.DataValueField = "ClassId";
+                ddlClass.DataBind();
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int subjectId = Convert.ToInt32(hfSubjectId.Value);
+            int boardId = Convert.ToInt32(ddlBoard.SelectedValue);
+            int? classId = null;
+
+            if (BoardSupportsClasses(boardId))
+            {
+                if (ddlClass.SelectedIndex < 0)
+                {
+                    lblMessage.Text = "Please select class.";
+                    return;
+                }
+
+                classId = Convert.ToInt32(ddlClass.SelectedValue);
+            }
+
+            string subjectName = txtSubjectName.Text.Trim();
+            string slug = txtSlug.Text.Trim();
+            string description = txtDescription.Text.Trim();
+            bool isActive = chkIsActive.Checked;
+
+            if (IsSlugExists(slug, subjectId))
+            {
+                lblMessage.Text = "Slug already exists.";
+                return;
+            }
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = @"UPDATE Subjects
+                                 SET BoardId=@BoardId,
+                                     ClassId=@ClassId,
+                                     SubjectName=@SubjectName,
+                                     Slug=@Slug,
+                                     Description=@Description,
+                                     IsActive=@IsActive
+                                 WHERE SubjectId=@SubjectId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@SubjectId", subjectId);
+                cmd.Parameters.AddWithValue("@BoardId", boardId);
+                cmd.Parameters.AddWithValue("@ClassId", (object)classId ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@SubjectName", subjectName);
+                cmd.Parameters.AddWithValue("@Slug", slug);
+                cmd.Parameters.AddWithValue("@Description", description);
+                cmd.Parameters.AddWithValue("@IsActive", isActive);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            Response.Redirect("ManageSubjects.aspx");
+        }
+
+        private bool BoardSupportsClasses(int boardId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "SELECT HasClassLayer FROM Boards WHERE BoardId=@BoardId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@BoardId", boardId);
+
+                return Convert.ToBoolean(cmd.ExecuteScalar());
+            }
+        }
+
+        private bool IsSlugExists(string slug, int subjectId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = @"SELECT COUNT(*) FROM Subjects 
+                                 WHERE Slug=@Slug AND SubjectId!=@SubjectId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Slug", slug);
+                cmd.Parameters.AddWithValue("@SubjectId", subjectId);
+
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+    }
+}
