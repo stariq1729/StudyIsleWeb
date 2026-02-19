17f24e907a0f92228a21d3b3e05a3a7a6f4a439c
Tariq Manzar <stariq1729@gmail.com>
2026-02-19 14:22:32 +0530

    added classes folder and manage classes page

---
+++ Admin/Classes/ManageClasses.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="ManageClasses.aspx.cs" Inherits="StudyIsleWeb.Admin.Classes.ManageClasses" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+      <div class="d-flex justify-content-between align-items-center mb-3">
+        <h4 class="page-title">Manage Classes</h4>
+        <a href="AddClass.aspx" class="btn btn-primary">+ Add Class</a>
+    </div>
+
+    <asp:GridView ID="gvClasses" runat="server"
+        AutoGenerateColumns="False"
+        CssClass="table table-bordered table-striped"
+        HeaderStyle-CssClass="table-dark"
+        OnRowCommand="gvClasses_RowCommand">
+
+        <Columns>
+
+            <asp:BoundField DataField="ClassId" HeaderText="ID" />
+
+            <asp:BoundField DataField="BoardName" HeaderText="Board" />
+
+            <asp:BoundField DataField="ClassName" HeaderText="Class Name" />
+
+            <asp:BoundField DataField="DisplayOrder" HeaderText="Order" />
+
+            <asp:TemplateField HeaderText="Status">
+                <ItemTemplate>
+                    <asp:LinkButton ID="btnToggle" runat="server"
+                        CommandName="ToggleActive"
+                        CommandArgument='<%# Eval("ClassId") %>'
+                        CssClass='<%# Convert.ToBoolean(Eval("IsActive")) ? "badge bg-success text-white p-2" : "badge bg-danger text-white p-2" %>'>
+                        <%# Convert.ToBoolean(Eval("IsActive")) ? "Active" : "Inactive" %>
+                    </asp:LinkButton>
+                </ItemTemplate>
+            </asp:TemplateField>
+
+        </Columns>
+
+    </asp:GridView>
+</asp:Content>
---
+++ Admin/Classes/ManageClasses.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StudyIsleWeb.Admin.Classes
+{
+    public partial class ManageClasses : System.Web.UI.Page
+    {
+        private readonly string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadClasses();
+            }
+        }
+
+        private void LoadClasses()
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = @"SELECT c.ClassId,
+                                        b.BoardName,
+                                        c.ClassName,
+                                        c.DisplayOrder,
+                                        c.IsActive
+                                 FROM Classes c
+                                 INNER JOIN Boards b ON c.BoardId = b.BoardId";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvClasses.DataSource = dt;
+                gvClasses.DataBind();
+            }
+        }
+
+        protected void gvClasses_RowCommand(object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "ToggleActive")
+            {
+                int classId = Convert.ToInt32(e.CommandArgument);
+                ToggleClassStatus(classId);
+                LoadClasses();
+            }
+        }
+
+        private void ToggleClassStatus(int classId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "UPDATE Classes SET IsActive = ~IsActive WHERE ClassId=@ClassId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@ClassId", classId);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
