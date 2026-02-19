92b73796b8ee010ea52629aa881f8748e3a80164
Tariq Manzar <stariq1729@gmail.com>
2026-02-19 14:51:08 +0530

    added subject folder

---
+++ Admin/Subjects/ManageSubjects.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="ManageSubjects.aspx.cs" Inherits="StudyIsleWeb.Admin.Subjects.ManageSubjects" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+      <div class="d-flex justify-content-between align-items-center mb-3">
+        <h4 class="page-title">Manage Subjects</h4>
+        <a href="AddSubject.aspx" class="btn btn-primary">+ Add Subject</a>
+    </div>
+
+    <asp:GridView ID="gvSubjects" runat="server"
+        AutoGenerateColumns="False"
+        CssClass="table table-bordered table-striped"
+        HeaderStyle-CssClass="table-dark"
+        OnRowCommand="gvSubjects_RowCommand">
+
+        <Columns>
+
+            <asp:BoundField DataField="SubjectId" HeaderText="ID" />
+
+            <asp:BoundField DataField="BoardName" HeaderText="Board" />
+
+            <asp:BoundField DataField="ClassName" HeaderText="Class" />
+
+            <asp:BoundField DataField="SubjectName" HeaderText="Subject" />
+
+            <asp:TemplateField HeaderText="Status">
+                <ItemTemplate>
+                    <asp:LinkButton ID="btnToggle" runat="server"
+                        CommandName="ToggleActive"
+                        CommandArgument='<%# Eval("SubjectId") %>'
+                        CssClass='<%# Convert.ToBoolean(Eval("IsActive")) ? "badge bg-success text-white p-2" : "badge bg-danger text-white p-2" %>'>
+                        <%# Convert.ToBoolean(Eval("IsActive")) ? "Active" : "Inactive" %>
+                    </asp:LinkButton>
+                </ItemTemplate>
+            </asp:TemplateField>
+
+        </Columns>
+
+    </asp:GridView>
+</asp:Content>
---
+++ Admin/Subjects/ManageSubjects.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StudyIsleWeb.Admin.Subjects
+{
+    public partial class ManageSubjects : System.Web.UI.Page
+    {
+        private readonly string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadSubjects();
+            }
+        }
+
+        private void LoadSubjects()
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = @"SELECT s.SubjectId,
+                                        b.BoardName,
+                                        c.ClassName,
+                                        s.SubjectName,
+                                        s.IsActive
+                                 FROM Subjects s
+                                 INNER JOIN Boards b ON s.BoardId = b.BoardId
+                                 LEFT JOIN Classes c ON s.ClassId = c.ClassId";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvSubjects.DataSource = dt;
+                gvSubjects.DataBind();
+            }
+        }
+
+        protected void gvSubjects_RowCommand(object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "ToggleActive")
+            {
+                int subjectId = Convert.ToInt32(e.CommandArgument);
+                ToggleSubjectStatus(subjectId);
+                LoadSubjects();
+            }
+        }
+
+        private void ToggleSubjectStatus(int subjectId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "UPDATE Subjects SET IsActive = ~IsActive WHERE SubjectId=@SubjectId";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@SubjectId", subjectId);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
