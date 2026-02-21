998f0e26efc24dbd19cfdef1bfd2b7a90e9a0371
Tariq Manzar <stariq1729@gmail.com>
2026-02-21 13:21:24 +0530

    added routeconfig.cs

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+
+namespace StudyIsleWeb
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.Ignore("{resource}.axd/{*pathInfo}");
+
+            // NCERT route
+            routes.MapPageRoute(
+                "NCERTRoute",
+                "ncert/{resourcetype}",
+                "~/NCERT.aspx"
+            );
+        }
+    }
+}
