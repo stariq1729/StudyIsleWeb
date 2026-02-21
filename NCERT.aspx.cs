b73e3e9cd70bf9573d1d0268f667042c4640fb6c
Tariq Manzar <stariq1729@gmail.com>
2026-02-21 13:12:15 +0530

    added dynamic cards

--- NCERT.aspx.cs
+++ NCERT.aspx.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace StudyIsleWeb
 {
@@ -11,7 +9,32 @@ namespace StudyIsleWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadResourceTypes();
+            }
+        }
 
+        private void LoadResourceTypes()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                string query = @"SELECT ResourceTypeId, TypeName, Slug
+                                 FROM ResourceTypes
+                                 WHERE IsActive = 1
+                                 ORDER BY DisplayOrder";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    rptResourceTypes.DataSource = reader;
+                    rptResourceTypes.DataBind();
+                }
+            }
         }
     }
 }
