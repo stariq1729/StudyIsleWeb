2f61dc43532bf6608ef5ac5c325c9e5c079faf69
Tariq Manzar <stariq1729@gmail.com>
2026-02-24 16:16:37 +0530

    changed resources typs

--- Admin/ResourceTypes/ManageResourceTypes.aspx.cs
+++ Admin/ResourceTypes/ManageResourceTypes.aspx.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Web.UI.WebControls;
 
 namespace StudyIsleWeb.Admin.ResourceTypes
 {
@@ -21,33 +22,48 @@ namespace StudyIsleWeb.Admin.ResourceTypes
         {
             using (SqlConnection con = new SqlConnection(cs))
             {
-                string query = "SELECT * FROM ResourceTypes ORDER BY CreatedAt DESC";
+                string query = @"SELECT 
+                                    ResourceTypeId,
+                                    TypeName,
+                                    Slug,
+                                    IsPremium,
+                                    IsActive,
+                                    DisplayOrder,
+                                    HasClass,
+                                    HasSubject,
+                                    HasChapter,
+                                    HasYear,
+                                    HasSubCategory
+                                 FROM ResourceTypes
+                                 ORDER BY DisplayOrder ASC";
 
-                using (SqlCommand cmd = new SqlCommand(query, con))
-                {
-                    con.Open();
-                    gvResourceTypes.DataSource = cmd.ExecuteReader();
-                    gvResourceTypes.DataBind();
-                }
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvResourceTypes.DataSource = dt;
+                gvResourceTypes.DataBind();
             }
         }
 
-        protected void gvResourceTypes_RowCommand(object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+        protected void gvResourceTypes_RowCommand(object sender, GridViewCommandEventArgs e)
         {
-            if (e.CommandName == "ToggleStatus")
+            if (e.CommandName == "ToggleActive")
             {
                 int id = Convert.ToInt32(e.CommandArgument);
 
                 using (SqlConnection con = new SqlConnection(cs))
                 {
-                    string query = "UPDATE ResourceTypes SET IsActive = ~IsActive WHERE ResourceTypeId = @Id";
+                    string query = @"UPDATE ResourceTypes
+                                     SET IsActive =
+                                     CASE WHEN IsActive = 1 THEN 0 ELSE 1 END
+                                     WHERE ResourceTypeId = @Id";
 
-                    using (SqlCommand cmd = new SqlCommand(query, con))
-                    {
-                        cmd.Parameters.AddWithValue("@Id", id);
-                        con.Open();
-                        cmd.ExecuteNonQuery();
-                    }
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Id", id);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
                 }
 
                 LoadResourceTypes();
