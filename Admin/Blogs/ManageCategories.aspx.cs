6d60e7b7cd053b932c39e4b0cb5933b487930038
Tariq Manzar <stariq1729@gmail.com>
2026-04-24 15:37:31 +0530

    created backend

--- Admin/Blogs/ManageCategories.aspx.cs
+++ Admin/Blogs/ManageCategories.aspx.cs
@@ -1,17 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace StudyIsleWeb.Admin.Blogs
 {
     public partial class ManageCategories : System.Web.UI.Page
     {
+        string connStr = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadCategories();
+            }
+        }
 
+        private void LoadCategories()
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                string query = "SELECT * FROM BlogCategories ORDER BY CreatedDate DESC";
+
+                using (SqlDataAdapter da = new SqlDataAdapter(query, con))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    gvCategories.DataSource = dt;
+                    gvCategories.DataBind();
+                }
+            }
+        }
+
+        protected void gvCategories_RowCommand(object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "DeleteCategory")
+            {
+                int categoryId = Convert.ToInt32(e.CommandArgument);
+
+                using (SqlConnection con = new SqlConnection(connStr))
+                {
+                    string query = "DELETE FROM BlogCategories WHERE CategoryId=@CategoryId";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@CategoryId", categoryId);
+
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+
+                LoadCategories();
+            }
         }
     }
 }
