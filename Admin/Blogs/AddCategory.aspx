0fabab53e99ccd3f413ef10d9dae4388f3067a1d
Tariq Manzar <stariq1729@gmail.com>
2026-04-24 15:32:04 +0530

    Designed category pages

--- Admin/Blogs/AddCategory.aspx
+++ Admin/Blogs/AddCategory.aspx
@@ -2,4 +2,29 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+<div class="container mt-4">
+    <h3>Add Blog Category</h3>
+
+    <div class="card p-4 mt-3">
+
+        <div class="mb-3">
+            <label>Category Name</label>
+            <asp:TextBox ID="txtCategoryName" runat="server" CssClass="form-control" />
+        </div>
+
+        <div class="mb-3">
+            <label>Type</label>
+            <asp:DropDownList ID="ddlType" runat="server" CssClass="form-control">
+                <asp:ListItem Text="General" Value="General" />
+                <asp:ListItem Text="Specific" Value="Specific" />
+            </asp:DropDownList>
+        </div>
+
+        <asp:Button ID="btnSave" runat="server" Text="Save Category" 
+            CssClass="btn btn-primary" OnClick="btnSave_Click" />
+
+    </div>
+</div>
+
 </asp:Content>
--- Admin/Blogs/AddCategory.aspx.cs
+++ Admin/Blogs/AddCategory.aspx.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace StudyIsleWeb.Admin.Blogs
 {
@@ -12,6 +9,39 @@ namespace StudyIsleWeb.Admin.Blogs
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            string categoryName = txtCategoryName.Text.Trim();
+            string type = ddlType.SelectedValue;
+
+            string slug = categoryName.ToLower().Replace(" ", "-");
+
+            string connStr = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                string query = @"INSERT INTO BlogCategories (CategoryName, Slug, Type)
+                                 VALUES (@CategoryName, @Slug, @Type)";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryName", categoryName);
+                    cmd.Parameters.AddWithValue("@Slug", slug);
+                    cmd.Parameters.AddWithValue("@Type", type);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+            // feedback
+            Response.Write("<script>alert('Category Added Successfully');</script>");
+
+            // clear fields
+            txtCategoryName.Text = "";
         }
     }
 }
